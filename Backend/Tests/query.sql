0e191fc94b219e2f7996950aa948cdf94508db87
William <william.allegret@efrei.net>
2024-06-24 23:27:43 +0200

    Prettier

---
+++ Backend/Tests/query.sql
@@ -0,0 +1,5 @@
+INSERT INTO utilisateurs (nom, cle_publique_rsa, cle_privee_rsa_chiffree, sel_mot_de_passe, mot_de_passe_hash) 
+VALUES 
+('Nicolas', 'cle_publique_rsa_user1', 'cle_privee_rsa_chiffree_user1', 'sel_user1', 'mot_de_passe_hash_user1'),
+('thomas', 'cle_publique_rsa_user2', 'cle_privee_rsa_chiffree_user2', 'sel_user2', 'mot_de_passe_hash_user2'),
+('william', 'cle_publique_rsa_user3', 'cle_privee_rsa_chiffree_user3', 'sel_user3', 'mot_de_passe_hash_user3');
--- Backend/query.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO utilisateurs (nom, email, cle_publique_rsa, cle_privee_rsa_chiffree, sel_mot_de_passe, mot_de_passe_hash) 
-VALUES 
-('Nicolas', 'nicolas@example.com', 'cle_publique_rsa_user1', 'cle_privee_rsa_chiffree_user1', 'sel_user1', 'mot_de_passe_hash_user1'),
-('thomas', 'user2@example.com', 'cle_publique_rsa_user2', 'cle_privee_rsa_chiffree_user2', 'sel_user2', 'mot_de_passe_hash_user2'),
-('william', 'william@example.com', 'cle_publique_rsa_user3', 'cle_privee_rsa_chiffree_user3', 'sel_user3', 'mot_de_passe_hash_user3');
